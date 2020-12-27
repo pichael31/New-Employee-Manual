ba1dfa9b8dea4f6c4f4ffe50a87c220561a050dd
pichael31 <44903985+pichael31@users.noreply.github.com>
2020-12-27 15:03:31 -0500

    Fix aspect ratio and Quit on escape

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -48,7 +48,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     public void SetState(GameState state)
